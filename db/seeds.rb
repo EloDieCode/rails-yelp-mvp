fe4b90edf6f6184db4fd086ff19c31c8eee7e5e0
Elodie Dalmon <elodie.dalmon@gmail.com>
2021-03-03 14:45:39 +0100

    yelp - sans mise en forme

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "04 72 00 00 01", category: "french"}
+
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "04 72 00 00 02", category: "italian"}
+
+bocuse = { name: "bocuse", address: "Collonges", phone_number: "04 72 00 00 03", category: "french" }
+
+shanghai = { name: "shanghai", address: "lyon 6", phone_number: "04 72 00 00 04", category: "chinese" }
+
+tokyo = { name: "tokyo", address: "lyon 4", phone_number: "04 72 00 00 05", category: "japanese" }
+
+
+[ dishoom, pizza_east, bocuse, shanghai, tokyo ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+puts "Finished!"
